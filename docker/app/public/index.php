2b788629f5acfc83b99b047b3e831807f8be6d9d
Marcos <marcaos.melo@gmail.com>
2024-06-18 21:03:10 +0100

    docker-php-mysql form style 1

--- docker/app/public/index.php
+++ docker/app/public/index.php
@@ -1,20 +1,23 @@
 <!DOCTYPE html>
 <html lang="pt-PT">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/style.css">
     <title>Sistema de Login</title>
 </head>
-<body>
-    <h1>Entrar</h1>
-    <form action="processa.php" method="POST">
-        <input type="email" placeholder="Utilizador" name="" id="">
-        <input type="password" placeholder="Senha" name="" id="">
-        <input type="submit" value="ACESSAR">
-        <a href="http://">Conheça meu trabalho!<strong> Inscreva-se</strong></a>
 
-    </form>
-    
+<body>
+    <div class="form-corpo">
+        <h1>Entrar</h1>
+        <form action="processa.php" method="POST">
+            <input type="email" placeholder="Utilizador">
+            <input type="password" placeholder="Senha">
+            <input type="submit" value="ACESSAR">
+            <a href="http://">Conheça meu trabalho!<strong> Inscreva-se</strong></a>
+        </form>
+    </div>
 </body>
+
 </html>
